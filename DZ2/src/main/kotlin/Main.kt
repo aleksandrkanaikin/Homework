94a72c92cf1cd344a271de662505f7413d6caefb
aleksandrkanaikin <99746818+aleksandrkanaikin@users.noreply.github.com>
2022-03-01 14:50:43 +0300

    Исправил

--- DZ2/src/main/kotlin/Main.kt
+++ DZ2/src/main/kotlin/Main.kt
@@ -1,9 +1,26 @@
 fun main(){
-    val ageArray = arrayOf(18, 22, 19, 38, 26)
-    val nameArray = arrayOf("Михаил", "Максим", "Николай", "Павел", "Светлана",)
-    val surNameArray = arrayOf("Терентьев", "Максимов", "Петров", "Семёнов", "Лебедева")
-    ageArray.sort()
-    for ((i, ages) in ageArray.withIndex()) {
-        println("Фамилия, Имя, Возраст: ${ageArray[i]} ${nameArray[i]} ${surNameArray[i]}")
+    var n = 0
+    val ageArray = Array(5){0}
+    val nameArray = Array<String?>(5){""}
+    val surnameArray = Array<String?>(5){""}
+    while(n < 5){
+        println("Введите фамилию")
+        val surname = readLine()
+        println("Введите имя")
+        val name = readLine()
+        println("Введите возраст")
+        val age : Int = readLine()?.toInt()!!
+        if ((age > 14) && (age < 18)) println("$name $surname не пускать в торговый центр")
+        if (age <= 14) println("$name $surname вызвать полицию")
+        if (age >= 18){
+            println("$name $surname пропустить")
+            ageArray[n] = age
+            nameArray[n] = name
+            surnameArray[n] = surname
+            n++
+        }
+    }
+    for((i) in ageArray.withIndex()) {
+        println("Возраст, Имя, Фамилия: ${ageArray[i]} ${nameArray[i]} ${surnameArray[i]}")
     }
 }
